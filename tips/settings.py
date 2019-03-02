e01cde2688446bea166bd6b5c3d2c0da67a1a704
Bob Belderbos <bobbelderbos@gmail.com>
2019-03-02 21:26:25 +0100

    change default permission

--- tips/settings.py
+++ tips/settings.py
@@ -121,10 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    )
 }
+
 
